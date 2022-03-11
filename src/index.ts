a04fd756327cf23bf856dc2d3a3030c5d32c8340
Mohammed Basyouni <mohmah345@gmail.com>
2022-03-11 20:18:35 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express'
 import morgan from 'morgan'
 import * as dotenv from 'dotenv'
+import helmet from 'helmet'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.use(morgan('short'))
+app.use(helmet())
+app.use(morgan('dev'))
 
 app.get('/', (_req: Request, res: Response) => {
   res.json({
